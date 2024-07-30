a2841b847f7e0994f7f65e41ba34beebe8c86374
KMG <kkang45597@gmail.com>
2024-07-30 22:51:18 +0900

    class edit test

--- testproject/src/testproject/Main.java
+++ testproject/src/testproject/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("mingi Edit mingi -> main");
+		System.out.println("mingi Edit mingi -> main checking");
 
 	}
 
